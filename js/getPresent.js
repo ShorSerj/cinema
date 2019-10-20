0b7960beffda42f217025d353d92914ff5294dc9
SerShor <shorser8@gmail.com>
2019-10-20 18:29:20 +0300

    <3.7>-<1>-<mobile version>

---
+++ js/getPresent.js
@@ -0,0 +1,9 @@
+let name_guest = document.getElementById('name_guests')
+let button_send_info = document.getElementById('sendPresentInfo')
+
+console.log('До нажатия кнопки отправить', name_guest.value)
+
+button_send_info.onclick = function(){
+    let name = name_guest.value
+    console.log('Имя', name)
+}
