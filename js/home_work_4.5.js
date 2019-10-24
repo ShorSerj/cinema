287cd7cbe215aa8f15caaa2552b4caa2c66396fc
SerShor <shorser8@gmail.com>
2019-10-24 20:33:59 +0300

    <4.5>-<1>-<trouble with task 3>

---
+++ js/home_work_4.5.js
@@ -0,0 +1,36 @@
+// let arr = [231, 865, false, 'margin']
+// console.log(arr)
+
+// a3 = {
+//     3: 'hello',
+//     'one': 'hi',
+//     'testt': 'vodoley',
+//     'ivan': 'ivanov'
+// };
+// for (let key in a3) {
+//     if (`${key}`.length > 4) {
+//         exit = `${key} - ${a3[key]}`
+//         // console.log(`${key} - ${a3[key]}`)
+//         let arr1 = document.getElementById('exit')
+//         arr1.innerHTML = exit
+//     }
+// }
+
+let a7 = new Object();
+let a8 = {};
+
+let spaceX = {
+    name: 'Falcon 1',
+    "first start": "28 setember 2008"
+}
+
+// a9 = String(document.getElementById("u7-key__input"))
+var u7_key__input = document.getElementById("u7-key__input")
+var u7_value__input = document.getElementById("u7-value__input")
+
+console.log(spaceX)
+spaceX.u7_key__input = u7_value__input
+
+console.log(spaceX)
+console.log(u7_key__input)
+console.log(u7_value__input)
