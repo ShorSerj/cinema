d65d421ccce82ae5691172a74bea069ee3a64417
SerShor <shorser8@gmail.com>
2019-10-26 23:14:27 +0300

    <4.6>-<1>-<comlete all tasks>

--- js/home_work_4.6.js
+++ js/home_work_4.6.js
@@ -1,5 +1,41 @@
+// Task 1
 function r1 () {
-    let name = document.getElementById("raz").value
-    output.innerHTML = name
+    let name = document.getElementById("raz").value;
+    output.innerHTML = name;
 }
+
+// Task 2
+function r2 (){
+    let first = document.getElementById("first").value;
+    let second = document.getElementById("second").value;
+    if(+first > +second){
+        max.innerHTML = first;
+    }
+    else{
+        max.innerHTML = second;
+    }
+}
+// Task 4
+function r4 (){
+    let first = document.getElementById("left").value;
+    let second = document.getElementById("right").value;
+    if(first > second){
+        var max = first;
+        var min = second;
+        console.log(max)
+        console.log(min)        
+    }
+    else{
+        var max = second;
+        var min = first;
+        console.log(max)
+        console.log(min)     
+    }
+   random = Math.floor(Math.random() * (+max - +min)) + +min; 
+   number1.innerHTML = random
+}
+
+
+
+    
 
